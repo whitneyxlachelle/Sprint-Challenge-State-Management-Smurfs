349f59b470f6add25be9d1b9ac0a99db899cfc51
whitneyxlachelle <whiggins16@yahoo.com>
2020-03-08 19:12:55 -0500

    axios call

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,29 @@
-import React, { Component } from "react";
+import React, { useState, useEffect }  from "react";
 import "./App.css";
-class App extends Component {
-  render() {
+
+import axios from 'axios';
+
+const App = () =>  {
+  const [smurf, setSmurf] = useState([]);
+
+useEffect(() => {
+  axios
+  .get('http://localhost:3333/smurfs')
+  .then(response => {
+    console.log(response);
+    setSmurf(response.data);
+  })
+  .catch(error => {
+    console.log(error)
+  });
+}, []) 
+  
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+    
       </div>
     );
-  }
+  
 }
 
 export default App;
