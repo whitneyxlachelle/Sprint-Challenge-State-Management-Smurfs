aca914acac6cb9b414fc8b23d4876b0ae0846817
whitneyxlachelle <whiggins16@yahoo.com>
2020-03-08 20:04:36 -0500

    data is displaying

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,29 +1,38 @@
-import React, { useState, useEffect }  from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import axios from 'axios';
 
-const App = () =>  {
+import SmurfContext from '../contexts/SmurfContext';
+import SmurfList from './Characters/SmurfList';
+
+const App = () => {
   const [smurf, setSmurf] = useState([]);
 
-useEffect(() => {
-  axios
-  .get('http://localhost:3333/smurfs')
-  .then(response => {
-    console.log(response);
-    setSmurf(response.data);
-  })
-  .catch(error => {
-    console.log(error)
-  });
-}, []) 
-  
-    return (
-      <div className="App">
-    
-      </div>
-    );
-  
+  // const addSmurf = smurfs => {
+  //   setSmurf([...smurf, smurfs])
+  // };
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        //console.log(response);
+        setSmurf(response.data);
+      })
+      .catch(error => {
+        console.log(error)
+      });
+  }, [])
+
+  return (
+    <div className="App">
+      <SmurfContext.Provider value={smurf}>
+        <SmurfList />
+      </SmurfContext.Provider>
+    </div>
+  );
+
 }
 
 export default App;
